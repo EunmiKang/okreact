57b09c410238f853cb111b820ed64b4c02bac81b
hellokem26 <hellokem26@gmail.com>
2022-07-05 11:23:36 +0900

    okdata 프리티어 예제 추가

--- okdata/src/App.jsx
+++ okdata/src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css';
-import { Link } from 'react-router-dom';
+import "./App.css"
+import { Link } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <h1>Main</h1>
-      <Link to="/mypage">My Page</Link> |
-      <Link to="/ajaxtest">Ajax Test</Link>
+      <Link to="/mypage">My Page</Link> |<Link to="/ajaxtest">Ajax Test</Link>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
